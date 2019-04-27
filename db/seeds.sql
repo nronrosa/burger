8a38a47df79800daff8a366f756d91806ca226ff
Nitza <rondon.nitza@gmail.com>
2019-04-26 21:42:09 -0400

    Initial files

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Burger Seed file
+INSERT INTO burgers (burger_name) 
+VALUES ("bacon cheeseburger");
+INSERT INTO burgers (burger_name) 
+VALUES ("black bean burger");
+INSERT INTO burgers (burger_name) 
+VALUES ("chicken burger");
