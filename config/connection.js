bac4567ed7636a56f0e57131a499af0f4023753d
Nitza <rondon.nitza@gmail.com>
2019-04-30 21:34:07 -0400

    Updated connection.js to include code for jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,6 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "trilogy2",
-//   password: "password123",
-//   database: "burgers_db"
-// });
-
-
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
